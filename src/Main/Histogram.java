5a1b0fba62afc2575e0758d377aeeb40b6662a6a
Joaquin <Joaquin@192.168.1.35>
2020-10-21 10:13:45 +0100

    Kata 2 Tipo Generico

---
+++ src/Main/Histogram.java
@@ -0,0 +1,24 @@
+package Main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Histogram<T>{
+    private final T[] data;
+
+    public Histogram(T[] data) {
+        this.data = data;
+    }
+    
+    public T[] getData() {
+        return data;
+    }
+    
+    public Map<T,Integer> getHistogram(){
+        Map<T,Integer> map = new HashMap<>();
+        for (int i = 0; i < data.length; i++) {
+            map.put(data[i], map.containsKey(data[i])? map.get(data[i]) +1 : 1);
+        }
+        return map;
+    }
+}
